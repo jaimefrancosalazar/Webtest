6ff49d6cea4f84565eae9c79a84620a82de4154d
Jenkins <jaimefrancosalazar@gmail.com>
2020-06-11 16:23:21 -0500

    commit message

---
+++ source/documentation.md
@@ -0,0 +1,3 @@
+<meta http-equiv="refresh" content="0;URL='http://guides.emberjs.com/'">
+
+Redirecting to [http://guides.emberjs.com/](http://guides.emberjs.com/)
---
+++ source/javascripts/common-all.js
@@ -0,0 +1,7 @@
+//= require vendor/jquery.bxSlider.min
+//= require vendor/getDevicePixelRatio-min
+//= require app/back-to-top.js
+//= require app/docsearch.js
+//= require underscore
+//= require gmaps/google
+//= require bootstrap-sprockets
